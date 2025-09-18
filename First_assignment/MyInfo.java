7d56f82e9bcae41c841b7387749f192c266261b3
MugenyiDerrick <derrickmugenyi90@gmail.com>
2025-09-18 21:07:35 +0300

    Add files via upload
    class assignment 1

---
+++ First_assignment/MyInfo.java
@@ -0,0 +1,9 @@
+public class MyInfo{
+    public static void main(String[] args) {
+        String name="MUGENYI DERRICK";
+        String regNo="S24B23/073";
+
+        System.out.println("Name:"+name);
+        System.out.println("Registration Number:"+regNo);
+    }
+}
