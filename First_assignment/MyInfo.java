9325b413c706cd33d9391b1fc3449c37ff3b2fc0
MugenyiDerrick <derrickmugenyi90@gmail.com>
2025-09-22 20:58:48 +0300

    Delete First_assignment directory

--- First_assignment/MyInfo.java
+++
@@ -1,9 +0,0 @@
-public class MyInfo{
-    public static void main(String[] args) {
-        String name="MUGENYI DERRICK";
-        String regNo="S24B23/073";
-
-        System.out.println("Name:"+name);
-        System.out.println("Registration Number:"+regNo);
-    }
-}
